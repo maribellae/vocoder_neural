102d12550d0e65085636562a51c9102f0f3692d5
maribellae <71653479+maribellae@users.noreply.github.com>
2023-02-26 01:41:18 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,148 @@
+from torchinfo import summary
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa.display
+import pylab
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import librosa    
+import glob
+import logging
+import hashlib
+import sys
+import speechbrain
+import torch
+import torchaudio
+import sentencepiece
+from types import SimpleNamespace
+from torch.nn import SyncBatchNorm
+from torch.nn import DataParallel as DP
+from hyperpyyaml import load_hyperpyyaml
+from speechbrain.pretrained.fetching import fetch
+from speechbrain.dataio.preprocess import AudioNormalizer
+import torch.nn.functional as F
+from torch.nn.parallel import DistributedDataParallel as DDP
+from speechbrain.utils.data_utils import split_path
+from speechbrain.utils.distributed import run_on_main
+from speechbrain.dataio.batch import PaddedBatch, PaddedData
+from speechbrain.utils.data_pipeline import DataPipeline
+from speechbrain.utils.callchains import lengths_arg_exists
+from speechbrain.utils.superpowers import import_from_path
+import os
+import matplotlib
+import pylab
+import librosa
+import librosa.display
+import numpy as np
+from speechbrain.dataio.preprocess import AudioNormalizer
+import IPython.display as ipd
+from librosa.feature.spectral import melspectrogram
+from sklearn.preprocessing import MinMaxScaler
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+import glob
+import tqdm
+import torch
+import argparse
+import numpy as np
+import hparams as hp
+from stft import TacotronSTFT
+from utils.utils import read_wav_np
+from audio_processing import pitch
+from text import phonemes_to_sequence
+import pandas as pd
+
+
+import os
+#os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import matplotlib.pyplot as plt
+
+import unidecode
+import IPython.display as ipd
+import pickle as pkl
+import librosa
+from text import *
+import numpy as np
+import torch
+import hparams
+from modules.model import Model
+from text import phonemes_to_sequence
+from g2p_en import G2p
+from text.cleaners import punctuation_removers
+
+import os
+import glob
+import tqdm
+import torch
+import argparse
+import numpy as np
+import hparams as hp
+from stft import TacotronSTFT
+from utils.utils import read_wav_np
+from audio_processing import pitch
+
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import torch.optim as optim
+import torch.nn.functional as F
+import torchaudio
+import numpy as np
+
+import librosa
+import shutil
+from speechbrain.utils.data_utils import download_file
+from speechbrain.pretrained import EncoderDecoderASR
+
+
+import matplotlib
+
+import pylab
+import librosa
+import librosa.display
+import numpy as np
+
+import IPython
+import IPython.display as ipd
+
+import os, argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from modules.model import Model
+from modules.loss import TransformerLoss
+import hparams
+from text import *
+from utils.utils import *
+from utils.writer import get_writer
+from utils.plot_image import *
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 optimizer = torch.optim.Adam(  list(model3.parameters()) + list(net.parameters())  , lr=3e-4)
 
 criterion = torch.nn.MSELoss().to(device)
