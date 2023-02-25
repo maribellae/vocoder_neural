97a5d1b04e888bc23bf6dda044e516d054a21b70
maribellae <71653479+maribellae@users.noreply.github.com>
2023-02-25 22:28:21 +0300

    Create installs.py

---
+++ installs.py
@@ -0,0 +1,85 @@
+!pip install speechbrain
+!pip install torchinfo
+from speechbrain.pretrained import EncoderDecoderASR
+from torchinfo import summary
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa.display
+import pylab
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import librosa    
+import glob
+import logging
+import hashlib
+import sys
+import speechbrain
+import torch
+import torchaudio
+import sentencepiece
+from types import SimpleNamespace
+from torch.nn import SyncBatchNorm
+from torch.nn import DataParallel as DP
+from hyperpyyaml import load_hyperpyyaml
+from speechbrain.pretrained.fetching import fetch
+from speechbrain.dataio.preprocess import AudioNormalizer
+import torch.nn.functional as F
+from torch.nn.parallel import DistributedDataParallel as DDP
+from speechbrain.utils.data_utils import split_path
+from speechbrain.utils.distributed import run_on_main
+from speechbrain.dataio.batch import PaddedBatch, PaddedData
+from speechbrain.utils.data_pipeline import DataPipeline
+from speechbrain.utils.callchains import lengths_arg_exists
+from speechbrain.utils.superpowers import import_from_path
+import os
+import matplotlib
+import pylab
+import librosa
+import librosa.display
+import numpy as np
+from speechbrain.dataio.preprocess import AudioNormalizer
+import IPython.display as ipd
+from librosa.feature.spectral import melspectrogram
+from sklearn.preprocessing import MinMaxScaler
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+
+!git clone https://github.com/carankt/FastSpeech2
+%cd FastSpeech2
+import os
+#os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+!pip install unidecode
+!pip install g2p_en
+import unidecode
+import IPython.display as ipd
+import pickle as pkl
+import librosa
+from text import *
+import numpy as np
+import torch
+import hparams
+from modules.model import Model
+from text import phonemes_to_sequence
+from g2p_en import G2p
+from text.cleaners import punctuation_removers
+
+
+import shutil
+from speechbrain.utils.data_utils import download_file
+from speechbrain.pretrained import EncoderDecoderASR
+
+import shutil
+from speechbrain.utils.data_utils import download_file
+
+MINILIBRI_TEST_URL = "https://www.openslr.org/resources/12/test-clean.tar.gz"
+download_file(MINILIBRI_TEST_URL, 'test-clean.tar.gz')
+shutil.unpack_archive( 'test-clean.tar.gz', '.')
+from speechbrain.pretrained import EncoderDecoderASR
