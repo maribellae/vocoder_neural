d6e81e1ebce046e8d46fbb316e1fdb66d94093cc
maribellae <71653479+maribellae@users.noreply.github.com>
2023-04-25 16:44:15 +0300

    Add files via upload

--- train_right.py
+++ train_right.py
@@ -148,24 +148,24 @@ class TransformerLoss(nn.Module):
         mel_output, pitch_out, energy_out = pred
         mel, pitch, energy = target
         text_lengths, mel_lengths = lengths
-        
-        mel_size = torch.tensor([mel.shape[0]])
-
+       # print(mel_lengths)
         assert(mel_output.shape == mel.shape)                  #check for the shape
         assert(pitch_out.shape == pitch.shape)
         assert(energy_out.shape == energy.shape)
             
-        mel_mask = get_mask_from_lengths(mel_size) 
+        #mel_mask = ~get_mask_from_lengths(mel_lengths)        # same for pitch and energy
+        mel_mask = get_mask_from_lengths(torch.tensor([mel.shape[2]])) 
         mel_mask[:,:mel_lengths.item()] = True
-        duration_mask = ~get_mask_from_lengths(text_lengths)
+        #print(mel_mask)
+       # duration_mask = ~get_mask_from_lengths(text_lengths)
         mel_mask = mel_mask.to(device)
-        duration_mask = duration_mask.to(device)
-
+       # duration_mask = duration_mask.to(device)
 
         
         mel = mel.masked_select(mel_mask.unsqueeze(1))
+        #print(mel)
         mel_output = mel_output.masked_select(mel_mask.unsqueeze(1))
-        
+        #print(mel_output)
         pitch = pitch.masked_select(mel_mask.unsqueeze(1))
         pitch_out = pitch_out.masked_select(mel_mask.unsqueeze(1))
         
@@ -201,70 +201,130 @@ def train(model3, device, data_len ,data, mels,lengths,  criterion, optimizer,it
     r = list(range(data_len))
     random.shuffle(r)
     for i in r:
-        word, mel,pitch, energy ,leen = torch.tensor(data[i][0]).to(torch.float32), mels[i].to(torch.float32) ,torch.tensor(data[i][2]).to(torch.float32), torch.tensor(data[i][2]).to(torch.float32),lengths[i]
-        len = torch.tensor([leen])  
+        word, mel,pitch, energy ,leen = torch.tensor(data[i][0]).to(torch.float32), mels[i].to(torch.float32) ,torch.tensor(data[i][2]).to(torch.float32), torch.tensor(data[i][3]).to(torch.float32),lengths[i]
+        len = torch.tensor([leen]) 
+       # print(len ,"LEN") 
         word, mel,pitch, energy,len = word[:,None].to(device),mel[None,:,:].to(device),pitch[None,:].to(device) , energy[None,:].to(device),len.to(device)
-     
-        model3.train()
+      #  print(word)
+        
         optimizer.zero_grad()
-
+       # model3.train()
+      #  print("ELEMENT " ,i )
+     #   word.requires_grad_()
+     #   mel.requires_grad_()
         with torch.enable_grad():
              mel_out, pitch_out,energy_out,length = model3(word, mel,len)
-
+      #  print ("FROM MODEL", mel_out.shape , pitch_out.shape,  energy_out.shape)
+     #   print(length , "LENGTH")
+      #  print(len,"LEN")
         mel_cut = mel[:,:,:len]
-
+      #  print(mel_out.requires_grad)
+      #  print(pitch_out.requires_grad)
+      #  print(energy_out.requires_grad)
+      #  print(mel_cut.shape)
+       # print ("FROM ref", mel_cut.shape , pitch.shape,  energy.shape)
         if (mel_cut.size(2)- mel_out.size(2))<0 :
-     
-            origin =mel_cut.size(2)
-            delta = mel_out.size(2)-mel_cut.size(2) -1
-         
-            mel_cut = torch.nn.functional.pad(mel_cut ,  (1, delta), "constant", 0)
-            delta = pitch_out.size(1) - pitch.size(1) - 1 
-          
+          #  print("A")
+           # origin =mel_cut.size(2)
+            delta = mel_out.size(2)-mel_cut.size(2) 
+           # print(delta) 
+            mel_cut = torch.nn.functional.pad(mel_cut ,  (0, delta), "constant", 0)
+            delta = pitch_out.size(1) - pitch.size(1)  
+          #  print(delta) 
             pitch = pitch[None,:,:]
             energy = energy[None,:,:]
             
-            pitch = torch.nn.functional.pad(pitch ,  (1, delta), "constant", 0)
-            energy = torch.nn.functional.pad(energy,  (1, delta), "constant", 0)
+            pitch = torch.nn.functional.pad(pitch ,  (0, delta), "constant", 0)
+            energy = torch.nn.functional.pad(energy,  (0, delta), "constant", 0)
             pitch = pitch.squeeze(0)
             energy = energy.squeeze(0)
         elif (mel_cut.size(2)- mel_out.size(2))>0 :
-           
-            origin = mel_cut.size(2)
-            delta = mel_cut.size(2) - mel_out.size(2)-1
-           
-            mel_out = torch.nn.functional.pad(mel_out ,  (1, delta), "constant", 0)
-            delta = pitch.size(1) - pitch_out.size(1) - 1 
-          
+            #print("B")
+            #origin = mel_cut.size(2)
+            delta = mel_cut.size(2) - mel_out.size(2)
+            #print(delta)
+            mel_out = torch.nn.functional.pad(mel_out ,  (0, delta), "constant", 0)
+            delta = pitch.size(1) - pitch_out.size(1) 
+            #print(delta)
             pitch_out= pitch_out[None,:,:]
             energy_out = energy_out[None,:,:]
-            pitch_out = torch.nn.functional.pad(pitch_out ,  (1, delta), "constant", 0)
-            energy_out = torch.nn.functional.pad(energy_out,  (1, delta), "constant", 0)
+            pitch_out = torch.nn.functional.pad(pitch_out ,  (0, delta), "constant", 0)
+            energy_out = torch.nn.functional.pad(energy_out,  (0, delta), "constant", 0)
             pitch_out = pitch_out.squeeze(0)
             energy_out = energy_out.squeeze(0)
         else:
             if (pitch.size(1)-pitch_out.size(1))<0 :
-                delta = pitch_out.size(1) - pitch.size(1) - 1 
-       
+                delta = pitch_out.size(1) - pitch.size(1)  
+          #  print(delta) 
                 pitch = pitch[None,:,:]
                 energy = energy[None,:,:]
             
-                pitch = torch.nn.functional.pad(pitch ,  (1, delta), "constant", 0)
-                energy = torch.nn.functional.pad(energy,  (1, delta), "constant", 0)
+                pitch = torch.nn.functional.pad(pitch ,  (0, delta), "constant", 0)
+                energy = torch.nn.functional.pad(energy,  (0, delta), "constant", 0)
                 pitch = pitch.squeeze(0)
                 energy = energy.squeeze(0)
+        #print(mel_cut.shape)
+        #print(mel_out, "ffgffffff")
+       # print("AFTER")
+       # print(mel_out.requires_grad)
+       # print(pitch_out.requires_grad)
+       # print(energy_out.requires_grad)
+      
+       # print ("FROM pad", mel_out.shape , pitch_out.shape,  energy_out.shape)
+        #print ("FROM pad ref", mel_cut.shape , pitch.shape,  energy.shape)
 
+      #  print (mel_out.requires_grad)
+      #  print(pitch_out.requires_grad)
+      #  print(energy_out.requires_grad)
         mel_loss,pitch_loss,energy_loss = criterion((mel_out, pitch_out, energy_out),(mel_cut,  pitch, energy),(torch.tensor([word.shape[0]]), torch.tensor([len])))
+      #  print (mel_out.requires_grad)
+      #  print(pitch_out.requires_grad)
+      #  print(energy_out.requires_grad)
+                
+        #loss = criterion(mel_out,  mel_cut) +  0.5*criterion(pitch_out,  pitch ) + 0.5*criterion(energy_out, energy)
+       # loss = criterion(mel_out,  mel_cut)
+        #losses.append(criterion(mel_output*mask,  mel[:,:,:length]).sum().item() / (80*itog))
+        loss = (mel_loss+0.5*pitch_loss+0.5*energy_loss)/(hparams.accumulation)
+      #  loss.backward()
+      #  losses.append(loss.item())
+ 
+       # model3.mylin3.weight.register_hook(lambda grad: print(grad))
 
-        loss = (mel_loss+0.4*pitch_loss+0.4*energy_loss)/(hparams.accumulation)
-
+      #  print (model3.mylin3.weight)
+       # model3.myconv1.weight.retain_grad
         loss.backward()
+      #  model3.myconv1.weight.retain_grad
+
+
+      #  print(loss.grad)
+       
+       # loss.backward(retain_graph = True)
+      #  print (model3.mylin3.weight.grad)
+
+       # print(model3.myconv1.weight.grad)
+       # print(model3.myconv2.weight.grad)
+       # print(model3.myfc2.weight.grad)
+       # print(model3.myfc3.weight.grad)
+
+     #   print(model3.Embedding.weight.grad)
+       # print(model3.mylin3.weight)
+     #   print(model3.Projection.weight.grad)
+        #for p in model3.parameters():
+        #    print(p.name)
+        #    print(p.grad)
+      #  print (
+        #gradient, *_ = model3.mylin3.weight.grad.data
+       
+        # print(gradient)
+       
+     #   model3.mylin3.weight.retain_grad()
+      #  print(model3.mylin3.weight.grad)
  
         optimizer.step()
         iter_meter.step()
         losses.append(loss.item())
         full_loss +=loss.item() 
-        
+   
              
     return(full_loss,losses)    
 
@@ -275,19 +335,22 @@ def check(model3, device, data_len ,data, mels,lengths,  iter_meter):
         word, mel,pitch, energy ,leen = torch.tensor(data[i][0]).to(torch.float32), mels[i].to(torch.float32) ,torch.tensor(data[i][2]).to(torch.float32), torch.tensor(data[i][2]).to(torch.float32),lengths[i]
         len = torch.tensor([leen])  
         word, mel,pitch, energy,len = word[:,None].to(device),mel[None,:,:].to(device),pitch[None,:].to(device) , energy[None,:].to(device),len.to(device)
- 
+      #  print(word)
+        
+
+      #  print("ELEMENT " ,i )
         mel_out, pitch_out,energy_out,length = model3(word, mel,len)
         
         mel_cut = mel[:,:,:len]
 
         if (mel_cut.size(2)- mel_out.size(2))<0 :
-       
+        #    print("A")
             origin =mel_cut.size(2)
             delta = mel_out.size(2)-mel_cut.size(2) -1
-          
+         #   print(delta) 
             mel_cut = torch.nn.functional.pad(mel_cut ,  (1, delta), "constant", 0)
             delta = pitch_out.size(1) - pitch.size(1) - 1 
-          
+          #  print(delta) 
             pitch = pitch[None,:,:]
             energy = energy[None,:,:]
             
@@ -296,13 +359,13 @@ def check(model3, device, data_len ,data, mels,lengths,  iter_meter):
             pitch = pitch.squeeze(0)
             energy = energy.squeeze(0)
         elif (mel_cut.size(2)- mel_out.size(2))>0 :
-           
+           # print("B")
             origin = mel_cut.size(2)
             delta = mel_cut.size(2) - mel_out.size(2)-1
-            
+            #print(delta)
             mel_out = torch.nn.functional.pad(mel_out ,  (1, delta), "constant", 0)
             delta = pitch.size(1) - pitch_out.size(1) - 1 
-           
+            #print(delta)
             pitch_out= pitch_out[None,:,:]
             energy_out = energy_out[None,:,:]
             pitch_out = torch.nn.functional.pad(pitch_out ,  (1, delta), "constant", 0)
@@ -312,7 +375,7 @@ def check(model3, device, data_len ,data, mels,lengths,  iter_meter):
         else:
             if (pitch.size(1)-pitch_out.size(1))<0 :
                 delta = pitch_out.size(1) - pitch.size(1) - 1 
-          
+          #  print(delta) 
                 pitch = pitch[None,:,:]
                 energy = energy[None,:,:]
             
@@ -327,27 +390,28 @@ def check(model3, device, data_len ,data, mels,lengths,  iter_meter):
 
 
         iter_meter.step()
-
+        
 
 
 iter_meter = IterMeter()
 
 criterion = TransformerLoss()
-optimizer = torch.optim.Adam(model3.parameters(),
+optimizer = torch.optim.Adam(list(model3.parameters()),
                                  lr=3e-4,
                                  betas=(0.9, 0.98),
                                  eps=1e-09)
-
+#print(model3)
 model3.train()
 
 epochs = 30
-
+#A = model3.mylin3.weight
+#B = model3.Projection.weight
 for epoch in range(1, epochs + 1):
       full_loss ,losses  = train(model3, device,2620,datas, mel_pad, input_lengths, criterion, optimizer,  iter_meter)
       print("Epoch  ",  epoch , "MEAN LOSS IS  " , full_loss/2620)
 
       if ((epoch  == 1)or(epoch  == 15)or(epoch == 30)) :
-           with open("/home/common/dorohin.sv/makarova/FastSpeech2/vocoder_neural/metrics/metrics_right_30_rewrite_{}.txt".format(epoch), "w") as output:
+           with open("/home/common/dorohin.sv/makarova/FastSpeech2/vocoder_neural/metrics/metrics_right_30_rewrite_paddings_{}.txt".format(epoch), "w") as output:
                 output.write(str(losses))
-           torch.save(model3.state_dict(), "/home/common/dorohin.sv/makarova/FastSpeech2/vocoder_neural/weights/weights_right_30_rewrite_{}".format(epoch))
+           torch.save(model3.state_dict(), "/home/common/dorohin.sv/makarova/FastSpeech2/vocoder_neural/weights/weights_right_30_rewrite_paddings_{}".format(epoch))
       
